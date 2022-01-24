a00d36617b44d739ef79d748429cad513ed2f4b3
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 15:38:21 +0100

    Change the order of the if condition in JS function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openCloseMenu() {
     var x = document.querySelector(".desktop-nav-bar");
-    if (x.style.display === "none")
-        x.style.display = "flex";
-    else 
+    if (x.style.display === "flex")
         x.style.display = "none";
+    else 
+        x.style.display = "flex";
 }
