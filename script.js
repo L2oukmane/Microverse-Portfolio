1086f819e1715d1b236d39421036e00b4d5fa294
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 15:34:14 +0100

    Implement JS function for mobile menu.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const menuOnion  =document.querySelector('.desktop-nav-bar');
-
-menuOnion.addEventListener('click',showMenu);
-
-function showMenu() {
-
+function openCloseMenu() {
+    var x = document.querySelector(".desktop-nav-bar");
+    if (x.style.display === "none")
+        x.style.display = "flex";
+    else 
+        x.style.display = "none";
 }
-
