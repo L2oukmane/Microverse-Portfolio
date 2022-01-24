b6833f4932dd9ca7e8570c1677d6371ff3ca3f56
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-24 17:59:10 +0100

    Fix contact me section.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function openCloseMenu() {
-  let x = document.querySelector('.desktop-nav-bar');
-  if (x.style.display === "flex"){
-  x.style.display = 'none';
+  const x = document.querySelector('.desktop-nav-bar');
+  const y = document.querySelector('body');
+  if (x.style.display === 'flex') {
+    x.style.display = 'none';
+    y.style.overflow = 'scroll';
   } else {
-  x.style.display = "flex";
-	}
+    x.style.display = 'flex';
+    y.style.overflow = 'hidden';
+  }
 }
-
-
-
-
+document.getElementById('onclick-open').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-close').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-portfolio').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-about').addEventListener('click', openCloseMenu);
+document.getElementById('onclick-contact').addEventListener('click', openCloseMenu);
