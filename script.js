0add6b5b9581fb57d74135c2b87a9b8eae2d3518
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-01-26 09:48:53 +0100

    Change the content of projectsCard function to be cleaner.

--- script.js
+++ script.js
@@ -60,67 +60,34 @@ function createProjectCards(){
   }
 }
 function projectCards(project,counter){
+  let toolsDiv = '';
+  for (let i=0;i<project.languages.length;i++){
+    toolsDiv += `<p>${project.languages[i]}</p>`;
+  }
   let container = document.querySelector('.grid-container');
   const workList = document.createElement('li');
   workList.classList.add('work');
   workList.classList.add('work'+counter);
-  let workImg = document.createElement('img');
-  workImg.classList.add('img1');
-  workImg.setAttribute('src',project.featuredImage);
-  workImg.setAttribute('alt','workSnapshot'+counter);
-  workList.appendChild(workImg); 
-  let descriptionDiv = document.createElement('div');
-  descriptionDiv.classList.add('work-descreption');
-  let heading1 = document.createElement('h1');
-  heading1.classList.add('tonic-class');
-  heading1.appendChild(document.createTextNode(project.heading1));
-  descriptionDiv.appendChild(heading1);
-  let canopy = document.createElement('div');
-  canopy.classList.add('canopy');
-  let heading3 = document.createElement('h3');
-  heading3.appendChild(document.createTextNode(project.heading2[0]));
-  let cardsList  =document.createElement('ul');
-  cardsList.classList.add('cards');
-  let list1 = document.createElement('li');
-  let list2 = document.createElement('li');
-  list1.classList.add('info');
-  list2.classList.add('info');
-  list1.appendChild(document.createTextNode(project.heading2[1]));
-  list2.appendChild(document.createTextNode(project.heading2[2]));
-  cardsList.appendChild(list1);
-  cardsList.appendChild(list2);
-  canopy.appendChild(heading3);
-  canopy.appendChild(cardsList);
-  descriptionDiv.appendChild(canopy);
-  let description = document.createElement('p');
-  description.classList.add('description');
-  description.appendChild(document.createTextNode(project.description));
-  descriptionDiv.appendChild(description);
-  let toolsDiv = document.createElement('div');
-  toolsDiv.classList.add('tools');
-  for (let i=0;i<project.languages.length;i++){
-    const language = document.createElement('p');
-    const languageText = document.createTextNode(project.languages[i]);
-    language.appendChild(languageText);
-    toolsDiv.appendChild(language);
-  }
-  descriptionDiv.appendChild(toolsDiv);
-  let viewDiv = document.createElement('div');
-  viewDiv.classList.add('view-project');
-  let viewBtn = document.createElement('button');
-  viewBtn.setAttribute('type', 'button');
-  viewBtn.appendChild(document.createTextNode('See Project'));
-  viewBtn.addEventListener('click', function() {
+  workList.innerHTML = `<img class="img1" src="${project.featuredImage}" alt="workSnapshot${counter}">
+                        <div class="work-descreption">
+                          <h1 class="tonic-class">${project.heading1}</h1>
+                          <div class="canopy">
+                            <h3>${project.heading2[0]}</h3>
+                            <ul class="cards">
+                              <li class="info">${project.heading2[1]}</li>
+                              <li class="info">${project.heading2[2]}</li>
+                            </ul>
+                          </div>
+                          <p class="description">${project.description}</p>
+                          <div class="tools">${toolsDiv}</div>
+                          <div class="view-project">
+                            <button class="project-button" type="button">See Project</button>
+                          </div>
+                        </div>`;
+  workList.querySelector('.project-button').addEventListener('click', () => {
     openClosePopup();
-    createPopUp(projects['project'+counter]);
-    console.log("test");
+    createPopUp(projects[`project${counter}`]);
   });
-  
-  viewDiv.appendChild(viewBtn);
-  descriptionDiv.appendChild(viewDiv);
-
-
-  workList.appendChild(descriptionDiv);
   container.appendChild(workList);
 }
 function openClosePopup(){
@@ -133,10 +100,11 @@ function openClosePopup(){
 }
 function createPopUp(project){
   const title = document.getElementById('heading1');
-  title.appendChild(document.createTextNode(project.heading1));
+  title.textContent = project.heading1;
   const heading3 = document.getElementById('heading3');
-  heading3.appendChild(document.createTextNode(project.heading2[0]));
+  heading3.textContent = project.heading2[0];
   const cards = document.getElementById('cardsList');
+  cards.innerHTML = '';
   let list1 = document.createElement('li');
   let list2 = document.createElement('li');
   list1.classList.add('info');
@@ -146,8 +114,9 @@ function createPopUp(project){
   cards.appendChild(list1);
   cards.appendChild(list2);
   const description = document.getElementById('description-text');
-  description.appendChild(document.createTextNode(project.description));
+  description.textContent = project.description;
   const projectTools = document.getElementById('project-tools');
+  projectTools.innerHTML = '';
   for (let i=0;i<project.languages.length;i++){
     const language = document.createElement('p');
     const languageText = document.createTextNode(project.languages[i]);
@@ -158,6 +127,8 @@ function createPopUp(project){
   liveButton.setAttribute('onclick',project.linkLive);
   const sourceButton = document.getElementById('source-button');
   sourceButton.setAttribute('onclick',project.linkSource);
+  const popupImage = document.getElementById('popUp-img');
+  popupImage.setAttribute('src', project.featuredImage);
 }
 document.addEventListener('DOMContentLoaded', function() {
   createProjectCards();
